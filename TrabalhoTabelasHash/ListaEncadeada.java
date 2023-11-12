b9807a1cebfe0e2a76154cce13a31b15108389fc
Victor S Camargo <107776635+VictorSilvaCamargo@users.noreply.github.com>
2023-11-12 20:26:27 -0300

    Add files via upload

---
+++ TrabalhoTabelasHash/ListaEncadeada.java
@@ -0,0 +1,30 @@
+public class ListaEncadeada {
+    No cabeca;
+
+    public ListaEncadeada() {
+        this.cabeca = null;
+    }
+
+    public void insere(int chave) {
+        No novoNo = new No(chave);
+        novoNo.proximo = cabeca;
+        cabeca = novoNo;
+    }
+
+    public int busca(int chave) {
+        No atual = cabeca;
+        int comparacoes = 0;
+        while (atual != null) {
+            comparacoes++;
+            if (atual.chave == chave) {
+                break;
+            }
+            atual = atual.proximo;
+        }
+        return comparacoes;
+    }
+
+    public boolean isEmpty() {
+        return cabeca == null;
+    }
+}
